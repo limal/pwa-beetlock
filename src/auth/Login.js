df3c89f510862788b95227892600b540649ca280
Lukasz Wolnik <lukasz@wolnik.co.uk>
2019-02-19 21:07:17 +0000

    Absolute import paths, login form

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,5 +1,57 @@
 import React from "react";
+import { Form, Field } from "react-final-form";
+import createFocusDecorator from "final-form-focus";
+import { required } from "common/forms/validators";
+import "../css/Auth.scss";
+
+const focusOnError = createFocusDecorator();
 
 export const Login = ({}) => {
-  return <div>Login</div>;
+  const onSubmit = values => {
+    window.alert(JSON.stringify(values, 0, 2));
+  };
+
+  return (
+    <div className="Login">
+      <h1 className="Login-Header">Login</h1>
+      <Form
+        onSubmit={onSubmit}
+        decorators={[focusOnError]}
+        render={({ handleSubmit, reset, submitting, pristine, values }) => (
+          <form className="Login-Form" onSubmit={handleSubmit}>
+            <Field name="email" validate={required}>
+              {({ input, meta }) => (
+                <div className="Input">
+                  <input
+                    {...input}
+                    type="text"
+                    placeholder="Email address"
+                    autoFocus
+                  />
+                  {meta.error && meta.touched && (
+                    <span className="Input-Error">{meta.error}</span>
+                  )}
+                </div>
+              )}
+            </Field>
+            <Field name="password" validate={required}>
+              {({ input, meta }) => (
+                <div className="Input">
+                  <input {...input} type="password" placeholder="Password" />
+                  {meta.error && meta.touched && (
+                    <span className="Input-Error">{meta.error}</span>
+                  )}
+                </div>
+              )}
+            </Field>
+            <div className="Login-Buttons">
+              <button className="Button" type="submit" disabled={submitting}>
+                Submit
+              </button>
+            </div>
+          </form>
+        )}
+      />
+    </div>
+  );
 };
--- src/common/Layout.js
+++ src/common/Layout.js
@@ -3,9 +3,12 @@ import "../css/Layout.scss";
 
 class Layout extends React.Component {
   render() {
+    const { children } = this.props;
+
     return (
       <div className="Layout">
         <div className="Layout-Header">WB LOCK</div>
+        {children}
       </div>
     );
   }
