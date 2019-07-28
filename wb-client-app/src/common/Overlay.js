8fd5855cea68f34ca74acea4ea64a089a354af19
Lukasz Wolnik <lukasz@wolnik.co.uk>
2019-07-28 16:28:52 +0100

    Name change

--- wb-client-app/src/common/Overlay.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const Overlay = ({ handleClick, open }) => {
-  return (
-    <div className={`Overlay ${!open && "Hidden"}`} onClick={handleClick} />
-  );
-};
--- wb-client-app/src/index.js
+++
@@ -1,8 +0,0 @@
-import "core-js/es6/map";
-import "core-js/es6/set";
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import { App } from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
