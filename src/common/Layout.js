3ef4536c9c2420cfc980cdf24dcd1a7bfe7b999e
Lukasz Wolnik <lukasz@wolnik.co.uk>
2019-07-29 10:15:52 +0100

    Change title

--- src/common/Layout.js
+++ src/common/Layout.js
@@ -9,7 +9,7 @@ const Layout = ({ children }) => {
     <div className="Layout">
       <div className="Layout-Header">
         {/* <Back className="Flip-H" /> */}
-        <h3>Beet Lock</h3>
+        <h3>Beet Lock HEY!</h3>
         <MainMenu />
       </div>
       {children}
